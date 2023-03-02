c118221826ecd39d9ff4abe160042ccd6be1567a
Denny <denisandreev1803@gmail.com>
2023-03-02 22:19:42 +0300

    DI modules commit

---
+++ app/src/main/java/com/dacoding/easyweather/di/LocationModule.kt
@@ -0,0 +1,20 @@
+package com.dacoding.easyweather.di
+
+import com.dacoding.easyweather.data.location.DefaultLocationTracker
+import com.dacoding.easyweather.domain.location.LocationTracker
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class LocationModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindLocationTracker(
+        defaultLocationTracker: DefaultLocationTracker
+    ): LocationTracker
+}
---
+++ app/src/main/java/com/dacoding/easyweather/di/RepositoryModule.kt
@@ -0,0 +1,19 @@
+package com.dacoding.easyweather.di
+
+import com.dacoding.easyweather.data.repository.WeatherRepositoryImpl
+import com.dacoding.easyweather.domain.repository.WeatherRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindWeatherRepository(
+        weatherRepositoryImpl: WeatherRepositoryImpl
+    ): WeatherRepository
+}
